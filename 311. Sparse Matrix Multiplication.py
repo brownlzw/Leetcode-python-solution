6da011c6cb9482d117b8a2704f2b57b3f8446c3a
Zhengwei Liu <zhengwei_liu@brown.edu>
2017-09-20 16:12:00 -0400

    311

---
+++ 311. Sparse Matrix Multiplication.py
@@ -0,0 +1,18 @@
+class Solution(object):
+  def multiply(self, A, B):
+    """
+    :type A: List[List[int]]
+    :type B: List[List[int]]
+    :rtype: List[List[int]]
+    """
+    d1, d2, d3 = len(A), len(B), len(B[0])
+    C = [[0]*d3 for _ in xrange(d1)]
+    for i in xrange(d1):
+        if not any(A[i]):
+            continue
+        for j in xrange(d2):
+            if A[i][j] == 0:
+                continue
+            for k in xrange(d3):
+                C[i][k] += A[i][j] * B[j][k]
+    return C
